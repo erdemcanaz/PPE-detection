5b866c8639595f06b3bc2da331a8a23f18dfd03b
Erdem <erdemcanaz@gmail.com>
2024-01-26 16:57:20 +0300

    Update split_to_frames.py

--- scripts/training/automating_scripts/split_to_frames.py
+++ scripts/training/automating_scripts/split_to_frames.py
@@ -1,19 +1,17 @@
 import time
 import cv2
 
-video_name_with_extension = 'Busy people walking the city streets in London HD Stock Footage.mp4'
-video_path = f"videos/{video_name_with_extension}"
+video_path = input("Please enter the video path to be splited into frames: ")
 save_path = "images/"
 
-def save_frame(video_path, save_path, skip_frames=1, delay_between_frames=0.025):
+def save_frame(video_path, save_path, skip_frames=30):
     """
     Play a video and save frames when 's' key is pressed.
 
     Args:
     video_path (str): Path to the video file.
     save_path (str): Directory where frames will be saved.
-    skip_frames (int): Number of frames to skip between checks for key press.
-    delay_between_frames (float): Number of seconds to wait between frames.
+    skip_frames (int): Number of frames to skip when 'a' or 'd' is pressed
     """
 
     print(video_path)
@@ -32,7 +30,7 @@ def save_frame(video_path, save_path, skip_frames=1, delay_between_frames=0.025)
         cv2.imshow('Frame', frame)
 
         # Check for 's' key press to save the frame
-        if cv2.waitKey(skip_frames):
+        if cv2.waitKey():
             if ord('s') == 0xFF:
                 frame_name = f"{save_path}/frame_{frame_counter}.jpg"
                 cv2.imwrite(frame_name, frame)
@@ -42,15 +40,14 @@ def save_frame(video_path, save_path, skip_frames=1, delay_between_frames=0.025)
                 break
 
             elif ord('d')== 0xFF:
-                frame_counter += 50 # set 450: if 30 FPS, then 15 seconds
+                frame_counter += skip_frames # set 450: if 30 FPS, then 15 seconds
                 if frame_counter >= total_frames:
                     break
             
             elif ord('a')== 0xFF:
-                frame_counter -= 450
+                frame_counter -= skip_frames
                 if frame_counter < 0:
                     frame_counter = 0
-
 
         frame_counter += 1
 
@@ -58,4 +55,4 @@ def save_frame(video_path, save_path, skip_frames=1, delay_between_frames=0.025)
     cv2.destroyAllWindows()
 
 # Usage
-save_frame(video_path, save_path, delay_between_frames= 0.25)
+save_frame(video_path=video_path, save_path=save_path, skip_frames = 30)
