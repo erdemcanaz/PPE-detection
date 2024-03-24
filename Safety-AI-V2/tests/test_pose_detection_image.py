3bfee05097f829c3dac9061cefd777ebe8e1ecd8
Erdem <erdemcanaz@gmail.com>
2024-03-25 01:16:45 +0300

    Update test_pose_detection_image.py

--- Safety-AI-V2/tests/test_pose_detection_image.py
+++ Safety-AI-V2/tests/test_pose_detection_image.py
@@ -1,6 +1,11 @@
+import torch
 from ultralytics import YOLO
 import cv2
-import pprint
+import pprint, time
+
+# Check for CUDA device and set it
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(f'Using device: {device}')
 
 yolo_models = [
     'yolov8n-pose.pt',
@@ -18,13 +23,18 @@ which_model_index = input("Which model do you want to use? (index):")
 which_model_index = int(which_model_index)
 which_model = yolo_models[which_model_index]
 
-model = YOLO(which_model)
+model = YOLO(which_model).to(device)
 
 # Load the image
 image = cv2.imread('pose_test_image.jpg')
 
 # Predict with the model
-results = model(image, show=True)
+start_time = time.time()
+for i in range(50):
+    results = model(image, show=True)
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f"FPS: {50/elapsed_time}")
 
 # Close the window
 cv2.destroyAllWindows()
