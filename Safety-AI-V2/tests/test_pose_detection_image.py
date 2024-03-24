30a7979282f505f01e7f3c4d3b8b82aa42c10e89
Erdem <erdemcanaz@gmail.com>
2024-03-25 01:09:58 +0300

    Update test_pose_detection_image.py

--- Safety-AI-V2/tests/test_pose_detection_image.py
+++ Safety-AI-V2/tests/test_pose_detection_image.py
@@ -1,7 +1,23 @@
 from ultralytics import YOLO
 import cv2
+import pprint
 
-model = YOLO('yolov8n-pose.pt')
+yolo_models = [
+    'yolov8s-pose.pt',
+    'yolov8m-pose.pt',
+    'yolov8l-pose.pt',
+    'yolov8x-pose.pt',
+    'yolov8x-pose-p6.pt',
+
+]
+
+pprint.pprint(yolo_models)
+
+which_model_index = input("Which model do you want to use? (index):")
+which_model_index = int(which_model_index)
+which_model = yolo_models[which_model_index]
+
+model = YOLO(which_model)
 
 # Load the image
 image = cv2.imread('pose_test_image.jpg')
